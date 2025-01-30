bf038e851f4bf1fcd1bf2186435ab0b70720d18b
Tom <107487368+Demoniarc@users.noreply.github.com>
2025-01-30 19:44:37 +0100

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -25,7 +25,7 @@ export default function Footer() {
               />
             </Link>
             <Link 
-              href="https://discord.gg/jHxTCBcg" 
+              href="https://discord.gg/kxbuMCbZRN" 
               target="_blank" 
               rel="noopener noreferrer"
             >
