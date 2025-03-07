5592c77671529fbd3636f0f78e562655a35a8eac
Tom <107487368+Demoniarc@users.noreply.github.com>
2025-03-07 14:58:53 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
                       alt={`${project.name} logo`}
                       width={48}
                       height={48}
-                      className="w-full h-full object-cover scale-100"
+                      className="w-full h-full object-cover scale-95"
                       sizes="48px"
                     />
                   </div>
