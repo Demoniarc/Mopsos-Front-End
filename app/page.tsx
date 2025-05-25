f1a39fc7c6d04677f1dc75e5f3fd6e91933f010b
Tom <107487368+Demoniarc@users.noreply.github.com>
2025-05-25 19:10:32 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
         const { data: projectsData, error: projectsError } = await supabase
           .from('project')
           .select('*')
-          .neq('id', 'fermion')
+          .not('id', 'in', ['natix', 'oceanprotocol', 'propbase', 'stronghold', 'bittensor', 'unizen', 'zebec', 'numerai', 'fermion', 'zigchain'])
 
         if (projectsError) throw projectsError
         if (projectsData) {
