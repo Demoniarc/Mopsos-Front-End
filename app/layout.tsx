ab66414f1014e036eb4a7b694e55abee1d256cec
Tom <107487368+Demoniarc@users.noreply.github.com>
2025-09-29 16:11:49 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,23 @@ export default function RootLayout({
     <html lang="fr" suppressHydrationWarning>
       <Head>
         <link rel="icon" href="/logo-dark.svg" type="image/svg+xml" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(){
+                if(!window.UnicornStudio){
+                  window.UnicornStudio={isInitialized:!1};
+                  var i=document.createElement("script");
+                  i.src="https://cdn.jsdelivr.net/gh/hiunicornstudio/unicornstudio.js@v1.4.31/dist/unicornStudio.umd.js",
+                  i.onload=function(){
+                    window.UnicornStudio.isInitialized||(UnicornStudio.init(),window.UnicornStudio.isInitialized=!0)
+                  },
+                  (document.head || document.body).appendChild(i)
+                }
+              }();
+            `
+          }}
+        />
       </Head>
       <body className={inter.className}>
         <ThemeProvider
